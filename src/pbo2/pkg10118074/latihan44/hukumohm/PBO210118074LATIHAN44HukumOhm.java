e0483e79c7d57f6dd5c4c33ec27a63a4755b1673
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-11-07 18:59:00 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118074/latihan44/hukumohm/PBO210118074LATIHAN44HukumOhm.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan44.hukumohm;
+
+/**
+ *
+ * @author ASUS
+ * NAMA : fikri maulana
+ * NIM : 1018074
+ * KELAS : if2
+ */
+public class PBO210118074LATIHAN44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    new Baterai();
+        Baterai baterai = new Baterai(3, 12);
+        
+        System.out.println("Developed by : fikri maulana");
+    }
+    
+}
+    
